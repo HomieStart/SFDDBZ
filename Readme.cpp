c778a41cdcf09571f6fabfe635658caff29fbba4
F-Homie <tr.x.homie@gmail.com>
2016-11-11 06:43:42 -0300

    Create Readme.cpp

---
+++ Readme.cpp
@@ -0,0 +1,11 @@
+/*
+''' -----------------------------------------------------------------------------
+''' Author   : Trx-Homie 
+''' Project  : SFD-MDS
+''' Github   : HomieStart
+''' License  : Creative Commons
+''' <summary>
+''' If you need the Doc or Need any Help, you can Contact me.
+''' </sumary>
+''' -----------------------------------------------------------------------------
+*/
